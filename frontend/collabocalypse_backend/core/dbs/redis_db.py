11e43468dfe58c276b1d3abeefb1538ecd917525
Shikha Mandal <shikhamandal2005@gmail.com>
2026-04-14 12:11:09 +0530

    Delete frontend/collabocalypse_backend directory

--- frontend/collabocalypse_backend/core/dbs/redis_db.py
+++
@@ -1,8 +0,0 @@
-import redis.asyncio as redis
-from core.config import settings
-
-
-redis_client = redis.from_url('redis://host.docker.internal:6379', decode_responses=False)
-#redis://localhost:6379
-#print(f"{settings.REDIS_URL}")
-#redis://host.docker.internal:6379
