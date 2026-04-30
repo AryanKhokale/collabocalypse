29d8434b4579fe0fe666287d26be381196b96745
AryanKhokale <aryan.k.20003@gmail.com>
2026-04-30 18:59:30 +0530

    Update Redis client URL to use settings.REDIS_URL

--- backend/core/dbs/redis_db.py
+++ backend/core/dbs/redis_db.py
@@ -2,7 +2,7 @@ import redis.asyncio as redis
 from core.config import settings
 
 
-redis_client = redis.from_url('redis://host.docker.internal:6379', decode_responses=False)
+redis_client = redis.from_url('settings.REDIS_URL', decode_responses=False)
 #redis://localhost:6379
 #print(f"{settings.REDIS_URL}")
 #redis://host.docker.internal:6379
