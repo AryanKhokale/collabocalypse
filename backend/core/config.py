0432b2fd2508d3840a1cc3e67d3f89037b31ddcd
AryanKhokale <aryan.k.20003@gmail.com>
2026-04-30 19:05:24 +0530

    Added new environment variables for Keycloak configuration.
    Added new environment variables for Keycloak configuration.

--- backend/core/config.py
+++ backend/core/config.py
@@ -7,6 +7,9 @@ class Settings:
     DATABASE_URL = os.getenv("DATABASE_URL")
     REDIS_URL = os.getenv("REDIS_URL")
     PASSWORD = os.getenv("PASSWORD")
+    KEYCLOAK_URL = os.getenv("KEYCLOAK_URL")
+    REALM = os.getenv("REALM")
+    CLIENT_ID = os.getenv("CLIENT_ID")
 
 settings = Settings()
 #print(f"DATABASE_URL={settings.DATABASE_URL}, REDIS_URL={settings.REDIS_URL}")
